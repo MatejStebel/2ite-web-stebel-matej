a08ca0bd3d4a96989ecd5a4eeb1d62f9cd44b866
MatejStebel <stebel.matej@gmail.com>
2026-01-01 20:15:47 +0100

    finished ring

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 const glow = document.getElementById('ring-glow');
 
 let t = 0;
@@ -6,14 +7,23 @@ let speed = 0.04;
 let minOpacity = 0.4;
 let maxOpacity = 0.7;
 
-let a = 0;
-const specificNumbers = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+let flicker = 0;
+let flickerRed = 0;
+
+let scrollDepth = 0;
 
 function animateGlow() {
     const amplitude = (maxOpacity - minOpacity) / 2;
     const mid = (maxOpacity + minOpacity) / 2;
+    const baseOpacity = mid + Math.sin(t) * amplitude;
 
-    glow.style.opacity = mid + Math.sin(t) * amplitude;
+    glow.style.opacity = baseOpacity + flicker;
+
+    glow.style.filter = `blur(35px) brightness(${1.4 + flickerRed * 2}) 
+    saturate(${1.25 + flickerRed * 2})`;
+    glow.style.boxShadow = `0 0 40px rgba(${255}, ${120 - flickerRed * 80}, ${0 + flickerRed * 120}, ${0.2 + flickerRed})`;
+    flicker *= 0.9;
+    flickerRed *= 0.88;
     t += speed;
 
     requestAnimationFrame(animateGlow);
@@ -22,23 +32,19 @@ function animateGlow() {
 animateGlow();
 
 window.addEventListener('scroll', () => {
-    const progress = window.scrollY / (document.body.scrollHeight - window.innerHeight);
-    a = progress;
+    const maxScroll = document.body.scrollHeight - window.innerHeight || 1;
 
-    const p = Math.min(Math.max(progress, 0), 1);
+    scrollDepth = window.scrollY / maxScroll;
+    const p = Math.min(Math.max(scrollDepth, 0), 1);
 
     minOpacity = 0.3 + p * 0.3;
     maxOpacity = 0.6 + p * 0.3;
-
     speed = 0.03 + p * 0.06;
 });
 
 setInterval(() => {
-    if (a > 0.8) {
-        const result = specificNumbers[Math.floor(Math.random() * specificNumbers.length)];
-        console.log(result);
-        if (result == 0.4) {
-            glow.style.opacity = 1;
-        }
+    if (scrollDepth > 0.8 && Math.random() < 0.15) {
+    flicker = 0.12;
+    flickerRed = 0.6;
     }
-}, 500);
+}, 450);
